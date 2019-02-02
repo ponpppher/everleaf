658685e17efae613a8e5d46cfe2dbcd1393aaaf2
ponpppher <genmaier@gmail.com>
2019-02-02 15:27:24 +0900

    add index and show pages

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,9 @@
 class Admin::UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -17,9 +22,7 @@ class Admin::UsersController < ApplicationController
   end
 
   def show
-  end
-
-  def index
+    @user = User.find(params[:id])
   end
 
   private
